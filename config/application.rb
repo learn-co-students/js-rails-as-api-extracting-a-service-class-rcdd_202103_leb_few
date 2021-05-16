0e99ec9b02b3734bf658c98a671187cae33c8647
Mira Chami <miramc149@gmail.com>
2021-05-16 20:17:55 +0000

    Done.

--- config/application.rb
+++ config/application.rb
@@ -38,3 +38,25 @@ module BirdSightingApi
     config.api_only = true
   end
 end
+
+mkdir app/services
+touch app/services/sighting_serializer.rb
+class SightingSerializer
+end
+class SightingsController < ApplicationController
+  def index
+    sightings = Sighting.all
+    render json: sightings.to_json(:include => {
+      :bird => {:only => [:name, :species]},
+      :location => {:only => [:latitude, :longitude]}
+    }, :except => [:updated_at])
+  end
+
+  def show
+    sighting = Sighting.find_by(id: params[:id])
+    render json: sighting.to_json(:include => {
+      :bird => {:only => [:name, :species]},
+      :location => {:only => [:latitude, :longitude]}
+    }, :except => [:updated_at])
+  end
+end
